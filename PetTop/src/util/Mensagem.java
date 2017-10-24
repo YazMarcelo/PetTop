a413f3275f765687d926cd93b85919c0e621e8df
HELM <marcellohugo01@gmail.com>
2017-10-24 19:12:27 -0200

    Criação da classe de utilitários

--- PetTop/src/util/Mensagem.java
+++ PetTop/src/util/Mensagem.java
@@ -19,18 +19,16 @@ public class Mensagem {
     1 = azul de informação
     2 = Amarelo de Atenção
     3 = cinza de questão
-    
-    
     */
     
     public void msg01(){
         JOptionPane.showMessageDialog(null, "Cadastro Efetuado com Sucesso!", "Mensagem do Sistema", 1);
     }
     public int msg02(){
-        return JOptionPane.showConfirmDialog(null, "Confirmar Alterações?", "Mensagem do Sistema", 3);
+        return JOptionPane.showConfirmDialog(null, "Confirmar Alterações?", "Mensagem do Sistema", 0, 3);
     }
     public int msg03(){
-        return JOptionPane.showConfirmDialog(null, "Confirmar Exclusão?", "Mensagem do Sistema", 3);
+        return JOptionPane.showConfirmDialog(null, "Confirmar Exclusão?", "Mensagem do Sistema", 0, 3);
     }
     public void msg04(){
         JOptionPane.showMessageDialog(null, "Alteração Efetuada com Sucesso!", "Mensagem do Sistema", 1);
@@ -49,5 +47,8 @@ public class Mensagem {
     }
     public void msg09(){
         JOptionPane.showMessageDialog(null, "Registro já existente!", "Mensagem do Sistema", 0);
+    }
+    public void msg10(){
+        JOptionPane.showMessageDialog(null, "CPF Inválido!", "Mensagem do Sistema", 0);
     }
 }
