8f4d7074e41a73f20ef3f333156bd1ff7823c5b8
aluno <aluno@LAB200-17>
2017-11-01 21:24:25 -0200

    Criação da interface de CRUD E criação do CRUD de Especie

---
+++ PetTop/src/interfaces/CRUD.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author aluno
+ */
+public interface CRUD {
+    void incluir(Object objeto)throws Exception;
+    void excluir(String id)throws Exception;
+    void alterar(Object objeto, String id)throws Exception;
+    public ArrayList<Object> listar()throws Exception;
+}
