dbdf423b734a7984545a85e4a00f560d681dc184
Marcelo <marcellohugo01@gmail.com>
2017-11-01 18:09:23 -0200

    criação da entidade de Cliente

--- PetTop/src/entidade/Cliente.java
+++ PetTop/src/entidade/Cliente.java
@@ -5,10 +5,94 @@
  */
 package entidade;
 
+import java.sql.Date;
+
 /**
  *
  * @author HELM
  */
 public class Cliente {
-    
+     private String nome = "";
+     private String rg = "";
+     private String cpf = "";
+     private Date dataNascimento;
+     private String telefone = "";
+     private String logradouro = "";
+     private String numero = "";
+     private String cep = "";
+     private String complemento = "";
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+     
+     
 }
