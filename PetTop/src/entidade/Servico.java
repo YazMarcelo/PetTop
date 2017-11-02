f1f01ffb28f24937265d5a7ca291253eae94163c
aluno <aluno@LAB200-12>
2017-11-01 22:07:38 -0200

    Criação do modelo de Serviço

---
+++ PetTop/src/entidade/Servico.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidade;
+
+/**
+ *
+ * @author aluno
+ */
+public class Servico {
+    
+    private int codigo = 0;
+    private String descricao = "";
+    private double valor = 0;
+
+    
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    
+    
+    
+}
